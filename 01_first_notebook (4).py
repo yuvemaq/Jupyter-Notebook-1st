93476a1cc977025940e909bd910ba0b0562c21dc
yuvemaq <78762744+yuvemaq@users.noreply.github.com>
2021-04-25 16:52:15 -0400

    Add files via upload

---
+++ 01_first_notebook (4).py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# "introduction"
+# 
+# "This is my _first_ Jupyter notebook"
+
+# In[4]:
+
+
+#used _first_
+
+
+# In[6]:
+
+
+x=5
+y=6
+
+z=x+y
+
+
+# In[8]:
+
+
+print(z)
+
+
+# In[ ]:
+
+
+
+
